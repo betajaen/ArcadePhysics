d264821a5c6192ff8615cf8e109380cd9915c003
Robin Southern <betajaen@ihoed.com>
2014-08-10 15:55:08 +0100

    Basic assets and wand movement for the Platformer Example

---
+++ Examples/Example Assets/Scripts/James.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class James : MonoBehaviour
+{
+
+  public GameObject wand;
+  public float wandDistance = 0.32f;
+
+  private Camera cam;
+  private Transform trans;
+  private Vector2 screenCoordsMe;
+  private float angle;
+  private Quaternion mWandRotation;
+
+  private const int kUp = 1;
+  private const int kDown = 2;
+  private const int kLeft = 4;
+  private const int kRight = 8;
+
+
+  void Start()
+  {
+    cam = Camera.main;
+    trans = transform;
+  }
+
+  void Update()
+  {
+
+  }
+
+  void FixedUpdate()
+  {
+    DoMove();
+    DoWand();
+  }
+
+  void DoMove()
+  {
+
+  }
+
+  void DoWand()
+  {
+
+    var sc = cam.ScreenToWorldPoint(Input.mousePosition);
+    var t = sc - trans.position;
+
+    var angle = Mathf.Atan2(t.y, t.x) * Mathf.Rad2Deg;
+    mWandRotation = Quaternion.AngleAxis(angle, new Vector3(0, 0, 1));
+
+    wand.transform.position = trans.position + mWandRotation * new Vector3(0.32f, 0.0f);
+    wand.transform.rotation = mWandRotation;
+  }
+
+
+
+}
---
+++ Examples/Example Assets/Scripts/PixelPerfectCamera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class PixelPerfectCamera : MonoBehaviour
+{
+  public float PixelsToUnits = 100.0f;
+  public float Zoom = 1.0f;
+
+  private Camera cam;
+  private float invPixelToUnits;
+
+  void Start()
+  {
+    invPixelToUnits = 1.0f / PixelsToUnits;
+    cam = GetComponent<Camera>();
+    cam.orthographicSize = Screen.height * 0.5f * invPixelToUnits / Zoom;
+  }
+
+  void Update()
+  {
+    cam.orthographicSize = Screen.height * 0.5f * invPixelToUnits / Zoom;
+  }
+
+}
